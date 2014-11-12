566f40ab3690e36d10c3c3068553c7dabdadbc12
Marcus Törnroth <m@rcus.se>
2014-11-13 00:13:22 +0100

    First commit

--- src/htmltable/CHTMLTable.php
+++ src/htmltable/CHTMLTable.php
@@ -3,12 +3,27 @@
 namespace rcus\HTMLTable;
 
 /**
- *
- *
+ * Makes nice tables in HTML with pagination.
  *
  */
 class CHTMLTable
 {
-    
-    
+    use Mos\Database\CDatabaseBasic;
+
+    /**
+     * Members
+     */
+    private $paginationDefault = 4;
+    private $paginationAlt     = array(2,4,8);
+
+
+
+
+    /**
+     * Constuctor
+     *
+     *
+     */
+
+
 }
--- webroot/testHTMLTable.php
+++ webroot/testHTMLTable.php
@@ -1,2 +1,18 @@
 <?php
 
+//
+// Set the error reporting.
+//
+error_reporting(-1);              // Report all type of errors
+ini_set('display_errors', 1);     // Display all errors
+ini_set('output_buffering', 0);   // Do not buffer outputs, write directly
+
+
+//
+// Get required files
+//
+require "../src/htmltable/CHTMLTable.php";
+require "../src/Database/TSQLQueryBuilderBasic.php";
+require "../src/Database/CDatabaseBasic.php";
+
+
